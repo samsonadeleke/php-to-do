41d4a6b1d8456d27f6aca6e61478c82845028fe8
Popsana Barida <popsyjunior@gmail.com>
2025-04-03 00:56:49 +0100

    chore: update styles

--- view/todo.html.php
+++ view/todo.html.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TODO App</title>
-    <link href="/assets/css/style.css" type="text/css" />
+    <link href="/assets/css/style.css" type="text/css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css" type="text/css" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
     <div class="page-content page-container" id="page-content">
